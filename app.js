b37e5f4453c2c7d861dd297a36990842e64c9ebb
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-09-25 16:37:04 -0400

    Small JS additions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Hamburger menu, hold the pickle.
 const hamburgerMenu = document.querySelector(".hamburgerMenu")
 const navList = document.querySelector(".navList")
 
@@ -10,3 +12,12 @@ document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", (
     hamburgerMenu.classList.remove("active")
     navList.classList.remove("active")
 }))
+
+// Shark facts buttons
+function sharkFact1(){
+    alert("Scientists can tell the age of a shark by checking the rings on it's vertebrae, just like trees!")
+}
+
+function sharkFact2(){
+    alert("It's thought that sharks first appeared 455 million years ago, making them older than trees!")
+}
