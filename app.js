7c7f3ce774a40a019c2fc693d03eeee6c8838a0b
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-09-26 10:52:00 -0400

    JS in progress for modal box.

--- app.js
+++ app.js
@@ -21,3 +21,40 @@ function sharkFact1(){
 function sharkFact2(){
     alert("It's thought that sharks first appeared 455 million years ago, making them older than trees!")
 }
+
+// modal button
+
+const openModal = document.querySelectorAll("[data-modal-target]");
+const closeModalButton = document.querySelectorAll("[data-modal-closer]");
+console.log()
+
+
+openModal.forEach(function (cButton){
+    cButton.addEventListener("click", function (){
+        const modal = document.querySelector(cButton.CDATA_SECTION_NODE.modalTarget);
+        myModal(modal);
+    });
+});
+
+function myModal(modal){
+    if (modal == null){
+        return;
+    }
+    modal.classList.add("show");
+    document.querySelector("body").classList.add("overlay");
+}
+
+closeModalButton.forEach(function (closeModalButton){
+    closeModalButton.addEventListener("click", function(){
+        const currentModal = closeModalButton.closest(".modal");
+        closeModalButton(currentModal);
+    });
+});
+
+function closeModal(currentModal){
+    if (currentModal == null){
+        return;
+    }
+    currentModal.classList.remove("show");
+    document.querySelector("body").classList.remove("overlay")
+}
